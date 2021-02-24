796fb6bbbf745f950596e88106921526a1de27e5
Ted Schelble <ted.h.schelble@gmail.com>
2021-02-23 20:26:36 -0800

    thirdpartyAPI lab complete

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,40 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
+  $(function(){
+    $(".judge-img").bind("taphold", tapholdHandler);
+
+    function tapholdHandler(event){
+      // get the id of the event source
+      var targetIDPrefix = event.target.id;
+      console.log("got prefix: " + targetIDPrefix);
+      // show bio
+      $("#" + targetIDPrefix + "-bio").show();
+    }
+  });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function(e) {
+    e.preventDefault();
+    console.log("submitting form...");
+    var rsvpEmail = $('#rsvpEmail').val();
+    // Send the post request
+    $.post('addRSVP', { rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res){
+    alert("RSVP form successfully submitted!");
+    $("#rsvpEmail").val(''); // clear form
+  }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+  // API Key
+  L.mapquest.key = '3yDpnVVbmkYeAJ4u8z8SjKBKtmWAbYG0';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.878714002777485, -117.23596110428323],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  // Add a marker
+  L.marker([32.88, -117.236]).addTo(map);
 }
